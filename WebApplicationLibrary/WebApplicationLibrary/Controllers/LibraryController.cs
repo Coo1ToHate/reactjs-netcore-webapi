39b982b4657d2582f7b9991f68ffbeba87a41b13
Coo1ToHate <pidexiniterid@mail.ru>
2022-02-11 10:52:09 +0700

    add NetCoreWebAPI

---
+++ WebApplicationLibrary/WebApplicationLibrary/Controllers/LibraryController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationLibrary.Data;
+using WebApplicationLibrary.Models;
+
+namespace WebApplicationLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LibraryController(DataContext context)
+        {
+            _context = context;
+            if (!context.Books.Any())
+            {
+                context.Add(new Book { Author = "Прайс Марк", Genre = "Учебная литература", Name = "C# 9 и .NET 5. Разработка и оптимизация", Year = 2022 });
+                context.Add(new Book { Author = "Carl Rippon", Genre = "Учебная литература", Name = "ASP.NET Core 3 and React", Year = 2019 });
+                context.Add(new Book { Author = "Дэн Браун", Genre = "Роман", Name = "Ангелы и демоны", Year = 2000 });
+                context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest("Bad Request");
+            }
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return Ok("Added Successfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest("Bad Request");
+            }
+
+            if (!_context.Books.Any(x => x.Id == book.Id))
+            {
+                return NotFound("NotFound");
+            }
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok("Update Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return BadRequest("Bad Request");
+            }
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ WebApplicationLibrary/WebApplicationLibrary/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApplicationLibrary.Data;
+
+namespace WebApplicationLibrary
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
